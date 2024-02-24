7337ea0a054132a1f389c3d3fe8b63aed963c868
Sharodh <sharodh25@gmail.com>
2024-02-24 19:01:30 +0530

    Kick and graceful termination added

--- client.py
+++ client.py
@@ -3,8 +3,8 @@ import threading
 import random
 import netifaces
 import sys
+import time
 
-#To get the ip addr of en0
 addrs = netifaces.ifaddresses('en0')
 ip_addr = addrs[netifaces.AF_INET][0]['addr']
 
@@ -13,66 +13,43 @@ client.bind(("localhost" , random.randint(8000,9000)))
 
 name = input("Nickname :")
 
+
 def receive():
-    while True:
+    global cond
+    while cond:
         try:
             message, _ = client.recvfrom(1024)
             sys.stdout.write('\r')
-            print(message.decode())
+            if message.decode().startswith("Kicked:"):
+                print(message.decode()[message.decode().index(" ")+1 :])
+                cond = False
+                client.close()
+                sys.exit()
+            else:
+                print(message.decode())
         except:
             pass
 
 
+cond = True
 t = threading.Thread(target=receive)
 t.start()
 
 client.sendto(f"SIGNUP_TAG:{name}".encode() , ("localhost",9999))
 
 while True:
-    message=input()
-    if message == "Exit":
-        client.sendto(f"exit:{name}".encode(),("localhost",9999))
-        exit()
-    elif message[0:5] == "Kick:":
-        message = message + " " +name
-        client.sendto(message.encode(),("localhost"),9999)
-        print("Request to kick has been sent to the admin")
-    else:
-        client.sendto(f"{name}: {message}".encode(), ("localhost",9999))
-
-
-
-def kick(message):
-    print(message.decode()[message.decode().index(":") + 2 :])
-    vote = input()
-    if(vote in ["yes" , "YES" , "Y" , "y" , "Yes"]):
-        client.sendto("1".encode(),client)
-    else:
-        client.sendto("0".encode(),client)
-    waiter() 
-         
-        
-def waiter():
-    while True: 
-        try:
-            message , _ = client.recvfrom(1024)
-            if(message.decode() == "You have been removed"):
-                print(message.decode())
-                exit()
-            else:
-                print(message.decode())
-        except:
-            pass
-        
-def get_ip_address():
     try:
-        addrs = netifaces.ifaddresses('en0')
-        if netifaces.AF_INET in addrs:
-            return addrs[netifaces.AF_INET][0]['addr']
+        message=input()
+        if message == "Exit":
+            client.sendto(f"exit:{name}".encode(),("localhost",9999))
+            cond = False
+            client.close()
+            sys.exit()
+        elif message[0:5] == "Kick:":
+            message = message + " " + name
+            client.sendto(message.encode(),("localhost", 9999))
+            print("Request to kick has been sent to the admin")
+        else:
+            client.sendto(f"{name}: {message}".encode(), ("localhost",9999))
     except:
-        return "Not found"
-
-
-t2 = threading.Thread(target=waiter)
-t2.start()
-
+        sys.exit()
--- server.py
+++ server.py
@@ -4,8 +4,7 @@ import queue
 
 messages = queue.Queue()
 clients = []
-client_2 = []
-
+client_2 = {}
 
 server = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 server.bind(("localhost",9999))
@@ -23,49 +22,52 @@ def broadcast():
     while True:
         while not messages.empty():
             message,addr = messages.get()
+            try:
+                to_be_kicked = message.decode()[message.decode().index("Kick:") + 2: message.decode().index("from")]
+            except:
+                pass
             print(message.decode())
             if addr not in clients:
                 clients.append(addr)
                 name = message.decode()[message.decode().index(":") + 1 : ]
-                temp_dict = dict()
-                temp_dict.update({name:addr})
-                client_2.append(temp_dict)
+                client_2.update({name:addr})
                 greeting(addr)
-                kick_v = len(clients)
             for client in clients:
                 try:
                     if client == addr:
                         continue
-                    if message.decode().startswith("SIGNUP_TAG"):
+                    elif message.decode().startswith("SIGNUP_TAG"):
                         name = message.decode()[message.decode().index(":")+1:]
                         server.sendto(f"{name} joined!".encode(),client)
                     elif message.decode().startswith("exit"):
                         name = message.decode()[message.decode().index(":")+1:]
                         server.sendto(f"{name} has left the Chatroom!".encode(),client)
                         clients.remove(client)
-                    # elif message.decode().startwith("Kick"):
-                    #     kick(message)
+                    elif message.decode().startswith("Kick"):
+                        list = message.decode().split()
+                        if client == client_2[list[1]]:
+                            server.sendto(f"Kicked: {list[-1]} has kicked you.".encode(),client)
+                            clients.remove[client_2[list[1]]] 
+                            del client_2[list[1]]
+                        else:
+                            server.sendto(f"{list[-1]} has kicked {list[1]}".encode(),client)
                     else:
                         server.sendto(message,client)
                 except:
                     clients.remove(client)
-                   
+
 t1 = threading.Thread(target=receive)
 t2 = threading.Thread(target=broadcast)
 
 t1.start()
 t2.start()
 
-
 def greeting(addr):
     mes = """Welcome to the Chatroom
     Instructions:-
     1.Please be friendly to everyone in the chatroom.
     2.To exit just type "Exit" <Enter>.
-    3.Remeber the admin can always kick you for misbehaving.
+    3.Remember the admin can always kick you for misbehaving.
     4.If you want to request to kick a user.
         Type Kick: <User_name>\n"""
     server.sendto(mes.encode(),addr)
-
-
-
